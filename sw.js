34ed6d13631221cb9d76149f263821b01c66d24b
Stefan Rammer <rammer.stefan@gmail.com>
2021-09-14 14:19:56 +0200

    first commit with saving date

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
-self.addEventListener('install', async event => {
-    console.log('install event')
-});
-  
-self.addEventListener('fetch', async event => {
-    console.log('fetch event')
-});
-
-const cacheName = 'pwa-conf-v1';
+const cacheName = 'cdt-conf-v1';
 const staticAssets = [
-  './',
   './index.html',
   './app.js',
   './styles.css'
 ];
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(req);
+    return cachedResponse || fetch(req);
+}
+
+self.addEventListener('install', async event => {
+    console.log('install event')
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+});
+  
+self.addEventListener('fetch', async event => {
+    console.log('fetch event')
+    const req = event.request;
+    event.respondWith(cacheFirst(req));
+});
+
+
+
